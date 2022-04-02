aa744a7bc15e9a674f3188c61a64df81ca5429f0
TahirMansuri <tprocks786@gmail.com>
2022-04-02 12:01:20 +0530

    MODIFIED: Calling URL by different REST API Calls

--- src/main/java/com/infogalaxy/springchatapp/controller/GreetingController.java
+++ src/main/java/com/infogalaxy/springchatapp/controller/GreetingController.java
@@ -1,15 +1,14 @@
 package com.infogalaxy.springchatapp.controller;
 
 import com.infogalaxy.springchatapp.model.Greeting;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 //Rest Controller for Handling all Greeting Message and Generate JSON Response
 
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
 
     //String template for generating greeting message
@@ -19,9 +18,29 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     //Mapping greeting with URL to generate the Bean Response in JSON Format
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name) {
+    //URL - http://localhost:8080/greeting/?name=Tahir
+    @RequestMapping(value={"","/"})
+    public Greeting greeting(@RequestParam(value="firstname", defaultValue = "World") String firstname) {
         return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+                String.format(template, firstname));
     }
+
+    //Passing firstname and lastname for user model
+    //URL - http://localhost:8080/greeting/user?firstname=Tahir&lastname=Mansuri
+    @RequestMapping("/user")
+    public Greeting greetingUser(@RequestParam(value="firstname", defaultValue = "Guest") String firstname,
+                             @RequestParam(value="lastname", defaultValue = "User") String lastname) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, (firstname+" "+lastname)));
+    }
+
+    //Passing firstname and lastname for user model using put method
+    //URL - {postman} localhost:8080/greeting/Tahir/?lastname=Mansuri
+    @PutMapping("/{firstname}")
+    public Greeting greeting(@PathVariable String firstname,
+                             @RequestParam(value = "lastname", defaultValue = "User") String lastname) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, (firstname+" "+lastname)));
+    }
+
 }
